57d176528bf45d186721118879013bfee4d5c741
gabriel <osbrin@gmail.com>
2017-08-28 01:55:34 -0500

    yay its done probable

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
 import requests
-import oauth2client
-import asyncio
-import websockets
+import urllib.parse as urllib
 import json
-from threading import Timer
+from timeit import default_timer as timer
 import time
 
-keys = open("keys.json", "r")
+keys = open("keys.json", "r").read()
+keys = json.loads(keys)
 
-OAUTH2_URL = "https://oauth.groupme.com/oauth/authorize?client_id="
 BASE_API_URL = "https://api.groupme.com/v3"
+TOKEN_URL  = BASE_API_URL + "/groups?token="
 
-AUTH_URL = OAUTH2_URL + keys.json()
+
+def event_checker():
+    start = timer()
+    events = open("events.json", "r").read()
+    events = json.loads(events)
+    keys = open("keys.json", "r").read()
+    keys = json.loads(keys)
+    today = time.strftime("%d%m%Y")
+    try:
+        events[today]
+        message = "Events today (" + time.strftime("%m/%d/%Y") + "):\n"
+        try:
+            events[today]["Day Type"]
+            for event in events[today]["Events"]:
+                new_message = event + " at " + events[today]["Events"][event]["Time"]
+                location = events[today]["Events"][event]["Location"]
+                message += new_message + ", located at " + location + "\n"
+                try:
+                    events[today]["Events"][event]["Itinerary"]
+                    message += "Itinerary:\n"
+                    for label in events[today]["Events"][event]["Itinerary"]:
+                        message += label + "\n"
+                except KeyError:
+                    pass
+                message += "\n"
+        except KeyError:
+            new_message = events[today]["Events"]
+            message += new_message
+        final_message = urllib.quote_plus(message)
+        print(final_message)
+        requests.post(BASE_API_URL + "/bots/post?bot_id=" + keys["bot_id"] + "&text=" + final_message)
+        end = timer()
+        print("Time: %s ms" % str((end - start)))
+    except KeyError:
+        string = "gabe pleas ubpate the events"
+        string = urllib.quote_plus(string)
+        requests.post(BASE_API_URL + "/bots/post?bot_id=" + keys["bot_id"] + "&text=" + string)
+
+event_checker()
