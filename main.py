0af7779a0fccd397ff66f1600449d2707a29aec0
gabriel <osbrin@gmail.com>
2017-08-28 13:47:03 -0500

    ok

--- main.py
+++ main.py
@@ -19,16 +19,13 @@ def event_checker():
     keys = json.loads(keys)
     today = time.strftime("%d%m%Y")
     try:
-        events[today]
         message = "Events today (" + time.strftime("%m/%d/%Y") + "):\n"
         try:
-            events[today]["Day Type"]
             for event in events[today]["Events"]:
                 new_message = event + " at " + events[today]["Events"][event]["Time"]
                 location = events[today]["Events"][event]["Location"]
                 message += new_message + ", located at " + location + "\n"
                 try:
-                    events[today]["Events"][event]["Itinerary"]
                     message += "Itinerary:\n"
                     for label in events[today]["Events"][event]["Itinerary"]:
                         message += label + "\n"
@@ -39,13 +36,14 @@ def event_checker():
             new_message = events[today]["Events"]
             message += new_message
         final_message = urllib.quote_plus(message)
-        print(final_message)
+        print(message)
         requests.post(BASE_API_URL + "/bots/post?bot_id=" + keys["bot_id"] + "&text=" + final_message)
-        end = timer()
-        print("Time: %s ms" % str((end - start)))
     except KeyError:
         string = "gabe pleas ubpate the events"
         string = urllib.quote_plus(string)
+        print(string)
         requests.post(BASE_API_URL + "/bots/post?bot_id=" + keys["bot_id"] + "&text=" + string)
+    end = timer()
+    print("Time: %s ms" % str((end - start)))
 
 event_checker()
